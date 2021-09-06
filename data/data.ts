27c21db91963579e953811fbc040c02b9fdd5529
wof <angel@watchoutfreedom.com>
2021-09-06 13:54:32 +0300

    data change

---
+++ data/data.ts
@@ -0,0 +1,91 @@
+const data = [
+    {
+        name: "Perritos en adopcion",
+        location: "Livorno, Italia",
+        tags: ["Animales", "Perritos", "Adopcion"],
+        url: "net/url",
+        avatar: "image/url.png",
+        description: "Net for animal rescue",
+        private_net: "private",
+        button_name: "pedidos",
+        button_type: {},
+        roles:["rol1","rol2"],
+        blocked_users:,
+        latitude: 43.33,
+        longitude: 43.33,
+        radius: 240,
+        friend_nets:[12,234],
+        net_modules:[0,10,23],
+        template:{},
+
+        buttons: [
+            {
+                type: 1,
+                tags: ["tag1", "tag2", "tag3"],
+                date: ["1/08/2021"],
+                location: ["Livorno, Italia"],
+                longitude: [43.2333],
+                latitude: [21.0002],
+                networks: [13,24],
+                is_group: true,
+                template: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
+            },
+            {
+                type: 1,
+                tags: ["tag1", "tag2", "tag3"],
+                date: ["1/08/2021"],
+                location: ["Livorno, Italia"],
+                longitude: [43.2333],
+                latitude: [21.0002],
+                networks: [13,24],
+                is_group: true,
+                template: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
+            },
+        ]
+    },
+    {
+        name: "Alimentos en Asturias",
+        location: "Livorno, Italia",
+        tags: ["Animales", "Perritos", "Adopcion"],
+        url: "net/url",
+        avatar: "image/url.png",
+        description: "Net for animal rescue",
+        private_net: "private",
+        button_name: "pedidos",
+        button_type: {},
+        roles:["rol1","rol2"],
+        blocked_users:,
+        latitude: 43.33,
+        longitude: 43.33,
+        radius: 240,
+        friend_nets:[12,234],
+        net_modules:[0,10,23],
+        template:{},
+        buttons: [
+            {
+                type: 1,
+                tags: ["tag1", "tag2", "tag3"],
+                date: ["1/08/2021"],
+                location: ["Livorno, Italia"],
+                longitude: [43.2333],
+                latitude: [21.0002],
+                networks: [13,24],
+                is_group: true,
+                template: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
+            },
+            {
+                type: 1,
+                tags: ["tag1", "tag2", "tag3"],
+                date: ["1/08/2021"],
+                location: ["Livorno, Italia"],
+                longitude: [43.2333],
+                latitude: [21.0002],
+                networks: [13,24],
+                is_group: true,
+                template: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
+            },
+        ]
+    },
+];
+
+export default fakeData;
