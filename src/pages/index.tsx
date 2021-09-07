1b1a0fb5ec7ee228e474e746112a004feee8cdcc
Albatroz Jeremias <ajeremias@disroot.org>
2021-09-07 16:15:47 +0100

    add axios example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import React, { useMemo } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Map from '../components/LeafletMap/index'
+import axios from "axios";
 
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
-
+  axios.get('http://localhost:3001/buttons/find').then((res) => {
+    res.data.map((button) => {
+      console.log(button.name);
+    });
+  }).catch(() => console.log('ERRRO'));
   const submitButton = async () => {
     const response = await fetch ('/api/buttons',{
       method: 'POST',
