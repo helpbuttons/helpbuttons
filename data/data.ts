e559a9dbf9c531606e9ff196c5719a39d89d57f5
wof <angel@watchoutfreedom.com>
2021-09-08 11:27:51 +0300

    data updated button area
    added a button area field in network data example, also changed for enum the chatType field

--- data/data.ts
+++ data/data.ts
@@ -1,14 +1,18 @@
 const data = [
     {
+        //required data
         name: "Perritos en adopcion",
-        location: "Livorno, Italia",
-        tags: ["Animales", "Perritos", "Adopcion"],
         url: "net/url",
         avatar: "image/url.png",
+        privacy: "publico", //enum {publico, privado} default publico
+        roles: "admin", //enum {admin, user, blocked} default admin, user
+        //not required data
+        tags: ["Animales", "Perritos", "Adopcion"],
         description: "Net for animal rescue",
-        privacy: "publico", //enum {publico, privado}
         buttonsTemplate: [{id:1,name:"repartidor",type:"offer",, fields:[geoCode:"posicion boton", , ]},{id:2,name:"restaurante",type:"need",, fields:[geoCode:"posicion boton", , ]}], //array of objects, each type has an int, a name and a color associated. Default are offer (green), need (red).
-        roles: "admin", //enum {admin, user, blocked}
+        //data for GIS
+        showButtons : "area", //enum {area, point} show buttons by area not showing exact position on map
+        location: "Livorno, Italia",
         latitude: 43.33,
         longitude: 43.33,
         radius: 240,
@@ -18,15 +22,18 @@ const data = [
         // extra option friendNets:[12,234],
 
         buttons: [
-            {
+            {   //required data
                 templateId: 1,
                 tags: ["tag1", "tag2", "tag3"],
+                //required data
                 date: ["1/08/2021"],
+                //GIS DATA
                 location: ["Livorno, Italia"],
                 longitude: [43.2333],
                 latitude: [21.0002],
+                // optional values
                 networks: [13,24],
-                isGroup: true,
+                chatType: "single", //enum {single,group} chat structure
                 templateExtraData: {}, //JSON template contains info about the image and the description (standard) and also about booleans, radius, checklist and every other field related to the network module
             },
             {
@@ -39,49 +46,6 @@ const data = [
                 networks: [13,24],
                 isGroup: true,
                 templateExtraData: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
-            },
-        ]
-    },
-    {
-        name: "Alimentos en Asturias",
-        location: "Livorno, Italia",
-        tags: ["Animales", "Perritos", "Adopcion"],
-        url: "net/url",
-        avatar: "image/url.png",
-        description: "Net for animal rescue",
-        private_net: "private",
-        button_name: "pedidos",
-        button_type: {},
-        roles:["rol1","rol2"],
-        blocked_users:,
-        latitude: 43.33,
-        longitude: 43.33,
-        radius: 240,
-        friend_nets:[12,234],
-        net_modules:[0,10,23],
-        template:{},
-        buttons: [
-            {
-                type: 1,
-                tags: ["tag1", "tag2", "tag3"],
-                date: ["1/08/2021"],
-                location: ["Livorno, Italia"],
-                longitude: [43.2333],
-                latitude: [21.0002],
-                networks: [13,24],
-                is_group: true,
-                template: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
-            },
-            {
-                type: 1,
-                tags: ["tag1", "tag2", "tag3"],
-                date: ["1/08/2021"],
-                location: ["Livorno, Italia"],
-                longitude: [43.2333],
-                latitude: [21.0002],
-                networks: [13,24],
-                is_group: true,
-                template: {}, //JSON template contains info about the image and the description (standard) and also about booleans, checklist and every other field related to the network module
             },
         ]
     },
