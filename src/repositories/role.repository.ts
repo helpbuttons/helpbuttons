75b7d2f6354bff5233ab99ca12e4579579848492
Albatroz Jeremias <ajeremias@disroot.org>
2021-09-20 14:36:15 +0100

    fix lint any error

--- src/repositories/role.repository.ts
+++ src/repositories/role.repository.ts
@@ -13,8 +13,10 @@ export class RoleRepository extends DefaultCrudRepository<
   ) {
     super(Role, dataSource);
 
+    /* eslint-disable @typescript-eslint/no-explicit-any */ 
     (this.modelClass as any).observe('persist', async (ctx: any) => {
       ctx.data.modified = new Date();
     });
+    /* eslint-enable @typescript-eslint/no-explicit-any */ 
   }
 }
--- src/repositories/template-button-network.repository.ts
+++ src/repositories/template-button-network.repository.ts
@@ -13,8 +13,10 @@ export class TemplateButtonNetworkRepository extends DefaultCrudRepository<
   ) {
     super(TemplateButtonNetwork, dataSource);
 
+    /* eslint-disable @typescript-eslint/no-explicit-any */ 
     (this.modelClass as any).observe('persist', async (ctx: any) => {
       ctx.data.modified = new Date();
     });
+    /* eslint-enable @typescript-eslint/no-explicit-any */ 
   }
 }
