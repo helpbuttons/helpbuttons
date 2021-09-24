af32bc47371ef9743ab9c922fdbcbd01d41d15f7
Albatroz Jeremias <ajeremias@disroot.org>
2021-09-24 01:13:02 +0100

    deactivate mail, only for production

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,42 +1,48 @@
 
+import { resolve } from "dns";
 import { NotificatioEmail } from "../types";
 require('dotenv').config()
+
+const nodemailer = require("nodemailer");
+
+const transportOpts = {
+  host: process.env.SMTP_HOSTNAME,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE == "true" ? true : false, // true for 465, false for other ports
+  auth: {
+    type: 'LOGIN',
+    user: process.env.SMTP_USER, // generated ethereal user
+    pass: process.env.SMTP_PASSWORD, // generated ethereal password
+  },
+};
+
+const transporter = nodemailer.createTransport(transportOpts);
+
 
 export class MailService {
 
   constructor() { }
 
-  
+
 
   async sendNotificationMail(notificationEmail: NotificatioEmail): Promise<object> {
-    const nodemailer = require("nodemailer");
-    
-    const transportOpts = {
-      host: process.env.SMTP_HOSTNAME,
-      port: process.env.SMTP_PORT,
-      secure: process.env.SMTP_SECURE == "true" ? true : false, // true for 465, false for other ports
-      auth: {
-        type: 'LOGIN',
-        user: process.env.SMTP_USER, // generated ethereal user
-        pass: process.env.SMTP_PASSWORD, // generated ethereal password
-      },
-    };
+    if (process.env.ENV == 'production') {
 
-    let transporter = nodemailer.createTransport(transportOpts);
-
-    // send mail with defined transport object
-    return await transporter.sendMail({
-      from: process.env.SMTP_FROM ? process.env.SMTP_FROM : 'Didnt configure the from environment var',
-      to: notificationEmail.to,
-      subject: notificationEmail.subject,
-      text: notificationEmail.content,
-      html: "<b>" + notificationEmail.content + "</b>",
-    }).then((info :any) => {
-      console.log("email sent! to %s subject %s", notificationEmail.to, notificationEmail.subject);
-      return info;
-    }).catch((err: any) => {
-      console.log(err);
-      console.log('failed to send email!!');
-    });;
+      // send mail with defined transport object
+      return await transporter.sendMail({
+        from: process.env.SMTP_FROM ? process.env.SMTP_FROM : 'Didnt configure the from environment var',
+        to: notificationEmail.to,
+        subject: notificationEmail.subject,
+        text: notificationEmail.content,
+        html: "<b>" + notificationEmail.content + "</b>",
+      }).then((info: any) => {
+        console.log("email sent! to %s subject %s", notificationEmail.to, notificationEmail.subject);
+        return info;
+      }).catch((err: any) => {
+        console.log(err);
+        console.log('failed to send email!!');
+      });
+    }
+    return Promise.resolve({});
   }
 }
