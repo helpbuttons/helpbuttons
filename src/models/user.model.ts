6535a66d763abddc727473e05afb30f3e86803d0
Albatroz Jeremias <ajeremias@disroot.org>
2021-09-27 16:22:45 +0100

    add test for mail validation

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -34,7 +34,12 @@ export class User extends Entity {
     type: 'string',
     required: true,
     unique: true,
-    pattern: '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/',
+    jsonSchema: {
+        type: "object",
+        propertyNames: {
+          format: "email"
+        }
+    }
   })
   email: string;
 
