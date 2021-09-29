cbb910244a9f975ce35cbd19b3907d5a1d39e344
wof <angel@watchoutfreedom.com>
2021-09-29 14:14:52 +0200

    template button services

--- src/services/ButtonTemplates/buttonTemplate.type.tsx
+++ src/services/ButtonTemplates/buttonTemplate.type.tsx
@@ -1,3 +1,3 @@
-export interface IButtonTemplate {
+export interface ITemplateButton {
 //JSON template contains info about the image and the description (standard) and also about booleans, radius, checklist and every other field related to the network module
 }
