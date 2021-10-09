e023981c942ea8fd327fcb197564898fd992cc33
Israel Saeta Pérez <dukebody@gmail.com>
2021-10-09 10:13:34 +0200

    Create Btn component with many props.
    In many cases I have put the Btn option next to the palin one in the Repository to allow comparison.
    
    Related to #23.

--- src/elements/Btn/index.tsx
+++ src/elements/Btn/index.tsx
@@ -1,53 +1,109 @@
 ///Btn is the project convention for tradittional buttons, in order to avoidd confussion with app's buttons
-import React, { Component } from "react";
+import React from "react";
 
 export enum BtnType {
-
-  ICON = "with-icon",
-  NOICON = "btn",
-
+    corporative,
+    splitIcon,
+    filter,
 }
 
-export interface BtnModel {
-
-    className: string;
+export enum IconType {
+    red,
+    green,
+    remove,
+    splitRedGreen,
+}
+export enum ContentAlignment {
+    left,
+    center,
+    right,
+}
+interface BtnProps {
     caption: string;
-    type: BtnType;
-    hasIcon: boolean
-
+    iconLeft?: IconType;
+    iconRight?: IconType;
+    contentAlignment?: ContentAlignment;
+    type?: BtnType;
+    disabled?: boolean;
 }
 
+function BtnIcon({ icon }: { icon: IconType }) {
+    switch (icon) {
+        case IconType.red:
+            return <div className="btn-filter__icon red"></div>;
+        case IconType.green:
+            return <div className="btn-filter__icon green"></div>;
+        case IconType.remove:
+            return <div className="btn-filter__remove-icon"></div>;
+        case IconType.splitRedGreen:
+            return (
+                <div className="btn-filter__split-icon">
+                    <div className="btn-filter__split-icon--half green-l"></div>
+                    <div className="btn-filter__split-icon--half red-r"></div>
+                </div>
+            );
+        default:
+            return null;
+    }
+}
 
-class Btn extends Component {
-  constructor(props) {
-    super(props)
-    let caption = 'caption'
+function CaptionNode({
+    caption,
+    hasIcon = false,
+}: {
+    caption: string;
+    hasIcon: boolean;
+}) {
+    if (hasIcon) {
+        return <div className="btn-with-icon__text">{caption}</div>;
+    } else {
+        return <>{caption}</>;
+    }
+}
 
-  }
+export default function Btn({
+    caption,
+    iconRight = null,
+    iconLeft = null,
+    type = null,
+    contentAlignment = null,
+    disabled = false,
+}: BtnProps) {
+    let classNames = [];
+    const hasIcon = iconRight !== null || iconLeft !== null;
 
-  render () {
+    switch (contentAlignment) {
+        case ContentAlignment.center:
+            classNames.push("btn--center");
+            break;
+    }
+
+    switch (type) {
+        case BtnType.corporative:
+            classNames.push("btn btn--corporative");
+            break;
+        case BtnType.filter:
+            if (hasIcon) {
+                classNames.push("btn-filter-with-icon");
+            } else {
+                classNames.push("btn-filter");
+            }
+            break;
+        default:
+            if (hasIcon) {
+                classNames.push("btn-with-icon");
+            } else {
+                classNames.push("btn");
+            }
+            break;
+    }
+    const className = classNames.join(" ");
+
     return (
-
-    );
-  }
-}
-
-
-
-export default function Btn(caption: string, hasIcon: boolean) {
-
-
-  return (
-
-      <>
-
-        <button className="{hasIcon ? 'btn' : 'btn-with-icon'}">
-
-          {caption}
-
+        <button disabled={disabled} className={className}>
+            <BtnIcon icon={iconLeft} />
+            <CaptionNode caption={caption} hasIcon={hasIcon} />
+            <BtnIcon icon={iconRight} />
         </button>
-
-      </>
-
-  );
+    );
 }
