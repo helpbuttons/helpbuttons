061d6c7f3087c418f6c1229762696f4f7554932b
wof <angel@watchoutfreedom.com>
2021-10-11 14:35:05 +0200

    change list elements to default state

--- src/components/list/CardButtonList/index.tsx
+++ src/components/list/CardButtonList/index.tsx
@@ -1,106 +1,87 @@
 //Card that displays a resume of the button info in tthe List component. Its fieldds can be customizedd according to buttonTemplate.
 import CrossIcon from '../../../../public/assets/svg/icons/cross1.tsx'
 
-export default function CardButtonList({button}) {
+export default function CardButtonList() {
   return (
     <>
-    <ul>
-      {button.map((button) => (
 
-        <li key={button.id}>
+          <div className="card-button-list card-button-list--need">
 
-          <Link
-            href={{
-              pathname: '/button/[slug]',
-              query: { slug: button.slug },
-            }}
-          >
+            <div className="card-button-list__picture-container">
 
-            <div className="card-button-list card-button-list--need">
+              <div className="card-button-list__nav">
 
-              <div className="card-button-list__picture-container">
-
-                <div className="card-button-list__nav">
-
-                  <div className="arrow btn-circle__icon">
-                    <CrossIcon />
-                  </div>
-                  <div className="arrow btn-circle__icon">
-                    <CrossIcon />
-                  </div>
-
+                <div className="arrow btn-circle__icon">
+                  <CrossIcon />
                 </div>
-
-                        <a>{button.name}</a>
-
-                <img src="https://dummyimage.com/1000/#ccc/fff" alt="button-picture" className="picture__img"></img>
+                <div className="arrow btn-circle__icon">
+                  <CrossIcon />
+                </div>
 
               </div>
 
-              <div className="card-button-list__content">
+                <a>Nombre</a>
 
-                <div className="card-button-list__header">
+              <img src="https://dummyimage.com/1000/#ccc/fff" alt="button-picture" className="picture__img"></img>
 
-                  <div className="card-button-list__info">
+            </div>
 
-                    <div className="card-button-list__status card-button-list__status">
+            <div className="card-button-list__content">
 
-                      <span className="card-button-list__status--offer">button type</span> y <span className="card-button-list__status--need">button type</span>
+              <div className="card-button-list__header">
 
+                <div className="card-button-list__info">
+
+                  <div className="card-button-list__status card-button-list__status">
+
+                    <span className="card-button-list__status--offer">button type</span> y <span className="card-button-list__status--need">button type</span>
+
+                  </div>
+
+                  <div className="card-button-list__status card-button-list__status">
+
+                    <span className="card-button-list__title">Button Name</span>
+
+                  </div>
+
+                </div>
+
+                <div className="card-button-list__submenu card-button-list__trigger">
+                </div>
+
+              </div>
+
+              <div className="card-button-list__hashtags">
+
+                    <div className="card-button-list__busca">
+                      <div className="hashtag">tag</div>
                     </div>
 
-                    <div className="card-button-list__status card-button-list__status">
+              </div>
 
-                      <span className="card-button-list__title">Button Name</span>
+              <div className="card-button-list__paragraph">
 
-                    </div>
+                <p>description</p>
 
-                  </div>
+                <p className="card-button-list__phone">phone</p>
 
-                  <div className="card-button-list__submenu card-button-list__trigger">
-                  </div>
+              </div>
 
+              <div className="card-button-lists">
+
+                <div className="card-button-list__city card-button-list__everywhere " >
+                  En todas partes
                 </div>
 
-                <div className="card-button-list__hashtags">
-
-                      <div className="card-button-list__busca">
-                        <div className="hashtag">tag</div>
-                      </div>
-
-                </div>
-
-                <div className="card-button-list__paragraph">
-
-                  <p>description</p>
-
-                  <p className="card-button-list__phone">phone</p>
-
-                </div>
-
-                <div className="card-button-lists">
-
-                  <div className="card-button-list__city card-button-list__everywhere " >
-                    En todas partes
-                  </div>
-
-                  <div className="card-button-list__date">
-                      Date
-                  </div>
-
+                <div className="card-button-list__date">
+                    Date
                 </div>
 
               </div>
 
             </div>
 
-          </Link>
-
-        </li>
-
-        ))}
-
-      </ul>
+          </div>
 
     </>
 
