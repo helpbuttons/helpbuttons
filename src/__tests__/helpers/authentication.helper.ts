f35d389e2758ff254a7ee7b931943c3713dcef4b
Albatroz Jeremias <ajeremias@disroot.org>
2022-03-21 17:01:56 +0000

    fix port... change to 80 so tests can run

--- src/__tests__/helpers/authentication.helper.ts
+++ src/__tests__/helpers/authentication.helper.ts
@@ -11,8 +11,8 @@ export async function setupApplication(): Promise<AppWithClient> {
     // Customize the server configuration here.
     // Empty values (undefined, '') will be ignored by the helper.
     //
-    // host: process.env.HOST,
-    // port: +process.env.PORT,
+    host: process.env.HOST,
+    port: 80,
   });
 
   const app = new HelpbuttonsBackendApp({
