fdd40cd7d5aa28489c8a436ab54f43c07dcb3ae7
Albatroz Jeremias <ajeremias@disroot.org>
2022-03-25 17:15:11 +0000

    setup nestjs, swagger and first crud users

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Helpbuttons backend')
+    .setDescription('.')
+    .setVersion('1.0')
+    .addTag('hb')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
