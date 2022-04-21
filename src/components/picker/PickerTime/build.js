c4ef883c6bf8099a39f33a4a9f28c1119b0a3d9e
ian <ianjosejf@gmail.com>
2022-04-21 20:20:18 +0200

    the calendar should be stylized and put logic of schedules

---
+++ src/components/picker/PickerTime/build.js
@@ -0,0 +1,15 @@
+export default function buildCalendar(value) {
+  const startDay = value.clone().startOf("month").startOf("week");
+  const endDay = value.clone().endOf("month").endOf("week");
+  const day = startDay.clone().subtract(1, "day");
+  const calendar = [];
+  while (day.isBefore(endDay, "day")) {
+    calendar.push(
+      Array(7)
+        .fill(0)
+        .map(() => day.add(1, "day").clone())
+    );
+  }
+
+  return calendar;
+}
--- src/components/picker/PickerTime/calendar.tsx
+++ src/components/picker/PickerTime/calendar.tsx
@@ -1,267 +1,86 @@
 import moment from "moment";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import buildCalendar from "./build";
+import dayStyles from "./styles";
 
 export default function Calendar() {
-  const [Days, setDays] = useState(null);
+  const [Days, setDays] = useState(moment());
+  const [calendar, setCalendar] = useState([]);
+  const [value, setValue] = useState(moment());
+
+  useEffect(() => {
+    const day = startDay.clone().subtract(1, "day");
+    const a = [];
+    while (day.isBefore(endDay, "day")) {
+      a.push(
+        Array(7)
+          .fill(0)
+          .map(() => day.add(1, "day").clone())
+      );
+    }
+    setCalendar(buildCalendar(value));
+  }, [value]);
+
+  const startDay = value.clone().startOf("month").startOf("week");
+  const endDay = value.clone().endOf("month").endOf("week");
+
+  function currMonthName() {
+    return value.format("MMMM");
+  }
+
+  function currYear() {
+    return value.format("YYYY");
+  }
+
+  function prevMonth() {
+    return value.clone().subtract(1, "month");
+  }
+
+  function nextMonth() {
+    return value.clone().add(1, "month");
+  }
+
+  console.log(value);
 
   return (
-    <section className="repository__section">
-      <div className="picker__content">
-        <div className="picker__section">
-          <header className="picker__header ">Selecciona día y hora</header>
-
-          <div className="picker__row">
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Lun</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mar</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Jue</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Vie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Sab</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Dom</div>
-              </div>
-            </button>
-          </div>
-
-          <div className="picker__row">
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Lun</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mar</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Jue</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Vie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Sab</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Dom</div>
-              </div>
-            </button>
-          </div>
-
-          <div className="picker__row">
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Lun</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mar</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Jue</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Vie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Sab</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Dom</div>
-              </div>
-            </button>
-          </div>
-
-          <div className="picker__row">
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Lun</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mar</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Jue</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Vie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Sab</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Dom</div>
-              </div>
-            </button>
-          </div>
-
-          <div className="picker__row">
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Lun</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mar</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Mie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Jue</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Vie</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Sab</div>
-              </div>
-            </button>
-
-            <button className="btn-circle">
-              <div className="btn-circle__content">
-                <div className="btn-circle__icon">Dom</div>
-              </div>
-            </button>
-          </div>
-
-          <div className="picker-time__selected">
-            <div className="picker-time__dropdown">
-              <div className="picker-time__dropdown-trigger">00</div>
-              <div className="picker-time__dropdown-content">
-                <div className="picker-time__dropdown-option">00</div>
-              </div>
-            </div>
-
-            <span className="picker-time__points">:</span>
-
-            <div className="picker-time__dropdown">
-              <div className="picker-time__dropdown-trigger">00</div>
-              <div className="picker-time__dropdown-content">
-                <div className="picker-time__dropdown-option">00</div>
-              </div>
-            </div>
-
-            <button className="picker-time__dropdown-option">AM</button>
-            <button className="picker-time__dropdown-option">PM</button>
-          </div>
+    <div className="calendar picker_content">
+      <div className="header">
+        <div
+          className="previous"
+          onClick={() => {
+            setValue(prevMonth());
+          }}
+        >
+          {String.fromCharCode(171)}
         </div>
-
-        <div className="picker__options-v">
-          <button
-            className="picker__option-btn--center"
-            type="button"
-            name="button"
-          >
-            <div className="picker__option-btn--txt">Aceptar</div>
-          </button>
+        <div className="current">
+          {currMonthName()} {currYear()}
+        </div>
+        <div
+          className="next"
+          onClick={() => {
+            setValue(nextMonth());
+          }}
+        >
+          {String.fromCharCode(187)}
         </div>
       </div>
-    </section>
+      <div className="body">
+        <div className="picker_section">
+          {calendar.map((week) => (
+            <div className="picker_row">
+              {week.map((day) => (
+                <div className="btn-circle day" onClick={() => setValue(day)}>
+                  <div className={dayStyles(day, value)}>
+                    {day.format("D").toString()}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
