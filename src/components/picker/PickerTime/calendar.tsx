eece53e82c412827e0a02a680bda35c572384157
ian <ianjosejf@gmail.com>
2022-04-22 18:53:20 +0200

    finish picker-time and calendar logic, missing calendar styles

--- src/components/picker/PickerTime/calendar.tsx
+++ src/components/picker/PickerTime/calendar.tsx
@@ -5,9 +5,10 @@ import buildCalendar from "./build";
 import dayStyles from "./styles";
 
 export default function Calendar() {
-  const [Days, setDays] = useState(moment());
   const [calendar, setCalendar] = useState([]);
   const [value, setValue] = useState(moment());
+  const [Hour, setHour] = useState("0");
+  const [Minutes, setMinutes] = useState("0");
 
   useEffect(() => {
     const day = startDay.clone().subtract(1, "day");
@@ -41,7 +42,20 @@ export default function Calendar() {
     return value.clone().add(1, "month");
   }
 
-  console.log(value);
+  const ParseIntMinutes = parseInt(Minutes);
+  const ParseIntHour = parseInt(Hour);
+
+  const CalendarioHoraDia = value.minutes(ParseIntMinutes).hours(ParseIntHour);
+
+  console.log(CalendarioHoraDia);
+
+  const updateHoras = function (event) {
+    setHour(event.target.value);
+  };
+
+  const updateMinutos = function (event) {
+    setMinutes(event.target.value);
+  };
 
   return (
     <div className="calendar picker_content">
@@ -80,6 +94,50 @@ export default function Calendar() {
             </div>
           ))}
         </div>
+      </div>
+      <div className="picker-time__selected">
+        <select
+          className="picker-time__dropdown picker-time__dropdown-option"
+          onChange={updateHoras}
+        >
+          <option value="00">00</option>
+          <option value="01">01</option>
+          <option value="02">02</option>
+          <option value="03">03</option>
+          <option value="04">04</option>
+          <option value="05">05</option>
+          <option value="06">06</option>
+          <option value="07">07</option>
+          <option value="08">08</option>
+          <option value="09">09</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="19">19</option>
+          <option value="20">20</option>
+          <option value="21">21</option>
+          <option value="22">22</option>
+          <option value="23">23</option>
+        </select>
+
+        <span className="picker-time__points">:</span>
+
+        <select
+          className="picker-time__dropdown picker-time__dropdown-option"
+          onChange={updateMinutos}
+        >
+          <option value="0">00</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+        </select>
       </div>
     </div>
   );
