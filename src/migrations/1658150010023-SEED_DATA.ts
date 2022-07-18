6d2f22c9b0cbe26963db83a57716117cada315b4
Albatroz Jeremias <ajeremias@disroot.org>
2022-07-18 16:00:43 +0100

    feat: seed, user, network and button!

--- src/migrations/1658150010023-SEED_DATA.ts
+++ src/migrations/1658150010023-SEED_DATA.ts
@@ -1,3 +1,5 @@
+import { Button } from "@src/modules/button/button.entity";
+import { Network } from "@src/modules/network/network.entity";
 import { UserCredential } from "@src/modules/user-credential/user-credential.entity";
 import { User } from "@src/modules/user/user.entity";
 import { dbIdGenerator } from "@src/shared/helpers/nanoid-generator.helper";
@@ -14,13 +16,13 @@ export class SEEDDATA1658150010023 implements MigrationInterface {
         const user = getRepository(User).create({
             id: userId,
             realm: '',
-            username: 'registered',
+            username: 'registered@email.com',
             verificationToken: 'notokeneeded',
             roles:  ['registered'],
             emailVerified: true,
         });
 
-        await getRepository(User).save(user);
+        let createdUser = await getRepository(User).save(user);
 
         const userCredential = getRepository(UserCredential).create({
             id: dbIdGenerator(),
@@ -30,8 +32,38 @@ export class SEEDDATA1658150010023 implements MigrationInterface {
 
         await getRepository(UserCredential).save(userCredential);
         // create new network
+        const network = getRepository(Network).create({
+            id: dbIdGenerator(),
+            name: 'Example Network',
+            description: 'this is an example network',
+            url: 'https://urlfornetwork.com',
+            radius: 10,
+            latitude: 38.86323,
+            longitude: -9.2697,
+            tags: ['tag1', 'tag2'],
+            location: () => `ST_MakePoint(38.86323, -9.2697)`,
+            avatar: '',
+        });
+
+        let createdNetwork = await getRepository(Network).save(network);
+
         // create button
 
+        const button = getRepository(Button).create({
+            id: dbIdGenerator(),
+            name: 'Example Button',
+            description: 'this is an example button',
+            latitude: 38.86323,
+            longitude: -9.2697,
+            tags: ['tag1', 'tag2'],
+            location: () => `ST_MakePoint(38.86323, -9.2697)`,
+            network: createdNetwork,
+            owner: createdUser,
+            images: [],
+            avatar: '',
+        });
+
+        await getRepository(Button).save(button);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
