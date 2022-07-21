1f76a473cf691a0448a1e2a32003526296ecec8f
Eva <eva.marco@kaleidos.net>
2022-07-21 10:56:28 +0200

    Change Login and Sign Up forms styles

--- src/elements/Fields/FieldPassword/index.tsx
+++ src/elements/Fields/FieldPassword/index.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import FieldError from "../FieldError";
 
+interface  FieldPassword {
+    label: string,
+    handleChange: Function,
+    name: string,
+    validationError: any,
+    classNameInput?: string,
+    placeholder?: string   
+}
+
 export default function FieldPassword({
     label,
     handleChange,
     name,
     validationError,
-}) {
+    classNameInput,
+    placeholder
+}: FieldPassword) {
     const onChange = (e) => {
         handleChange(name, e.target.value);
       };
 
     return (
         <div className="form__field">
-        <label className="label">{label}</label>
+            <label className="label">{label}</label>
             <input 
-                      name={name} 
-                      type="password"
-                      onChange={onChange}
-                      className={`form__input ${validationError ? 'validation-error' : ''}`} 
-                    />
+                name={name} 
+                type="password"
+                onChange={onChange}
+                className={`form__input ${classNameInput} ${validationError ? 'validation-error' : ''}`} 
+                placeholder={placeholder}
+            />
             <FieldError validationError={validationError}/>
         </div>
     );
