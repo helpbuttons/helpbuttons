7e04e381abbb29f37fc6d18d594971cb2fbc100a
Albatroz Jeremias <ajeremias@disroot.org>
2023-01-16 23:49:13 +0000

    feat: add password confirmation and username to signup form

--- web/src/components/user/NewUserFields/index.tsx
+++ web/src/components/user/NewUserFields/index.tsx
@@ -2,7 +2,7 @@ import FieldUploadImage from "elements/Fields/FieldImageUpload";
 import FieldPassword from "elements/Fields/FieldPassword";
 import FieldText from "elements/Fields/FieldText";
 
-export default function NewUserFields({register, errors, control, setValue}) {
+export default function NewUserFields({register, errors, control, setValue, watch}) {
   return (
     <>
       <FieldText
@@ -12,6 +12,14 @@ export default function NewUserFields({register, errors, control, setValue}) {
         placeholder="email@email.em"
         validationError={errors.email}
         {...register('email', { required: true })}
+      ></FieldText>
+      <FieldText
+        name="username"
+        label={`Username ${watch('username')}@${window.location.hostname}`}
+        classNameInput="squared"
+        placeholder="username"
+        validationError={errors.username}
+        {...register('name', { required: true })}
       ></FieldText>
       <FieldText
         name="name"
@@ -28,6 +36,14 @@ export default function NewUserFields({register, errors, control, setValue}) {
         placeholder="Type your password"
         validationError={errors.password}
         {...register('password', { required: true, minLength: 8 })}
+      ></FieldPassword>
+      <FieldPassword
+        name="password_confirm"
+        label="Password confirmation"
+        classNameInput="squared"
+        placeholder="Type your password again please"
+        validationError={errors.password}
+        {...register('password_confirm', { required: true, minLength: 8 })}
       ></FieldPassword>
       150x150px
       <FieldUploadImage
