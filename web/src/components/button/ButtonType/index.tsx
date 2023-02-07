7a93624e0bd22266822f4ddfdcd172a202fd9ba4
Albatroz Jeremias <ajeremias@disroot.org>
2023-02-07 18:58:29 +0000

    feat: add buttonTypes to json file

--- web/src/components/button/ButtonType/index.tsx
+++ web/src/components/button/ButtonType/index.tsx
@@ -1,58 +1,56 @@
 //This is the first section in the button creation process, but can be displayed or not depending on the selected network settings. It displays the buttons types (default offer an need and exchange) and leads to ButtonNewData after selection
-import FieldError from "elements/Fields/FieldError";
-import FieldRadio from "elements/Fields/FieldRadio";
-import FieldRadioOption from "elements/Fields/FieldRadio/option";
-import React from "react";
-import { useState } from "react";
+import FieldError from 'elements/Fields/FieldError';
+import FieldRadio from 'elements/Fields/FieldRadio';
+import FieldRadioOption from 'elements/Fields/FieldRadio/option';
+import React from 'react';
+import { useState } from 'react';
 
-import { IoClose } from "react-icons/io5";
-type IconType = "cross" | "red";
+import { IoClose } from 'react-icons/io5';
+import { buttonTypes } from 'shared/buttonTypes';
+type IconType = 'cross' | 'red';
 
 function RadioIcon({ icon }: { icon: IconType }) {
   switch (icon) {
-    case "cross":
+    case 'cross':
       return (
         <div className="checkbox__icon">
           <IoClose />
         </div>
       );
-    case "red":
+    case 'red':
       return <div className="btn-filter__icon red"></div>;
     default:
       return null;
   }
 }
 
-const ButtonType = React.forwardRef(({name, onChange, onBlur, validationError}, ref) => {
-  return (
-        <>
-          <FieldRadio label="Button type:">
-            <FieldRadioOption
-              onChange={onChange} 
-              onBlur={onBlur}
-              name={name}
-              ref={ref} 
-              value="need"
-            >
-              <div className="btn-filter__icon red"></div>
-              <div className="btn-with-icon__text">Need</div>
-            </FieldRadioOption>
-            <FieldRadioOption
-              onChange={onChange} 
-              onBlur={onBlur}
-              name={name}
-              ref={ref} 
-              value="offer"
-            >
-              <div className="btn-filter__icon green"></div>
-              <div className="btn-with-icon__text">Offer</div>
-            </FieldRadioOption>
-          </FieldRadio>
-          <FieldError validationError={validationError}/>
-        </>
-  )
-});
+const ButtonType = React.forwardRef(
+  ({ name, onChange, onBlur, validationError }, ref) => {
+    return (
+      <>
+        <FieldRadio label="Button type:">
+          {buttonTypes.map((buttonType) => (
+            <>
+              <FieldRadioOption
+                onChange={onChange}
+                onBlur={onBlur}
+                name={name}
+                ref={ref}
+                value={buttonType.name}
+              >
+              <div className={`btn-filter__icon ${buttonType.color}`}></div>
+              <div className="btn-with-icon__text">{buttonType.name}</div>
+              </FieldRadioOption>
+            </>
+            
+          ))}
+        </FieldRadio>
+        <FieldError validationError={validationError} />
+      </>
+    );
+  },
+);
 
-ButtonType.displayName = "ButtonType"
+ButtonType.displayName = 'ButtonType';
 
 export default ButtonType;
---
+++ web/src/shared/buttonTypes/index.tsx
@@ -0,0 +1,10 @@
+export const buttonTypes = [
+    {
+        name: "Offer",
+        color: "red",
+    },
+    {
+        name: "Need",
+        color: "green",
+    }
+];
