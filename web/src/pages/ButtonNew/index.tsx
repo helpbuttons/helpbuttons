0165d8038b2674376d352b4a3d6f97fa28f2d2c1
watchoutfreedom <watchoutfreedom@gmail.com>
2023-02-10 18:31:39 +0100

    fill faqs

--- web/src/pages/ButtonNew/index.tsx
+++ web/src/pages/ButtonNew/index.tsx
@@ -124,7 +124,7 @@ export default function ButtonNew() {
               <FieldTextArea
                 name="title"
                 label="Title"
-                placeholder="Abacates"
+                placeholder="Write a title for your button"
                 validationError={errors.title}
                 {...register('title', { required: true })}
               />
--- web/src/pages/Faqs/index.tsx
+++ web/src/pages/Faqs/index.tsx
@@ -15,21 +15,27 @@ export default function Faqs() {
         <div className="popup__section">
 
             <Accordion title="What's Helpbuttons">
+            Helpbuttons.org is an opensource platform for collaborative network making. It's a CMS for cooperative websites -like uber, glovo, blablacar and so many others- specialized in connecting unknown people by purposes in real life. 
             </Accordion>
 
             <Accordion title="What's for">
+              Shortly, we want to democratize collaborative-apps-creation and we need a big community for this to be sustainable in time.
             </Accordion>
 
-            <Accordion title="What's a button">
+            <Accordion title="What's a Button">
+              A Button is a post. Every post published by an user in Helpbuttons it´s called a button. Buttons can be offers, needs, exchanges, transport proposals, bussinesses, anything you want. 
             </Accordion>
 
-            <Accordion title="What's a Net">
+            <Accordion title="What's a Network">
+              A network is your instance of Helpbuttons. You can create your own network and assign it a purpose (shaare food, help animals, take care of elders,...) so you can start collaborating with whatever collective you want. 
             </Accordion>
 
             <Accordion title="The community">
+              The Helpbuttons community is what makes this project sustainable. This tool is made to be adapted to many purposes, so many people can both benefit and help to sustain the project. We don´t need an app for sharing food and a different app for sharing transport, we believe we can share the same core structure and adapt the same app to different purposes. In that way we could get more people together and make a better tool. 
             </Accordion>
 
             <Accordion title="Ethics Policies">
+              
             </Accordion>
 
             <Accordion title="Privacy Policies">
