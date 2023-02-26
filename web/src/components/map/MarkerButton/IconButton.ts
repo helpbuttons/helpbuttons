396302f6a5bb8ef8a9b3bf281058a2c9b36ef58f
Albatroz Jeremias <ajeremias@disroot.org>
2023-02-26 22:12:33 +0000

    feat: show gray marker, when sets of marker are not set

--- web/src/components/map/MarkerButton/IconButton.ts
+++ web/src/components/map/MarkerButton/IconButton.ts
@@ -1,31 +1,53 @@
 import L from 'leaflet';
 import { makeImageUrl } from 'shared/sys.helper';
 
-function MarkerIcon(title = '', markerImage = '', markerColor = 'red') {
-  markerImage = makeImageUrl(markerImage, '/api/');
-  return L.divIcon({
-      className: 'marker-button',
-      html:(
+function MarkerButtonImage({ markerImage, title }) {
+  if (markerImage) {
+    return `<img src="${markerImage}"
+  alt="${title}"
+  class="picture__img"/>`;
+  } else {
+    return ;
+  }
+}
 
-            `<figure id='markerButton' class="marker-button marker-button-selector ${markerColor}">
+function MarkerButtonTitle({ title }) {
+  if (title)
+  {
+    return `
+    <div class="marker-button__link-tag">
+                            ${title}
+                          </div>`;
+  }
+  return ;
+  
+}
+
+function MarkerIcon(
+  title = '',
+  markerImage = '',
+  markerColor = 'gray',
+) {
+  markerImage = makeImageUrl(markerImage, '/api/');
+  if (!markerColor) {
+    markerColor = 'gray';
+  }
+
+  return L.divIcon({
+    className: 'marker-button',
+    html: `<figure id='markerButton' class="marker-button marker-button-selector ${markerColor}">
                     <div class="avatar-medium marker-button__image">
-                    <img src="${markerImage}"
-                         alt="${title}"
-                         class="picture__img"/>
+                    <MarkerButtonImage markerImage={markerImage} title={title}/>
                     </div>
 
                     <span class="marker-button__arrow"></span>
 
                     <div class="marker-button__tags marker-button-selector-title ${markerColor}">
-                        <div class="marker-button__link-tag">
-                          ${title}
-                        </div>
+                      <MarkerButtonTitle title="${title}"/>
                     </div>
-              </figure>`
-
-            ),
-      iconSize: [30, 42],
-      iconAnchor: [15, 42]
+              </figure>`,
+    iconSize: [30, 42],
+    iconAnchor: [15, 42],
   });
 }
 
