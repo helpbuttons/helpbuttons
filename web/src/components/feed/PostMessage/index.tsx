1593d8ee28aca47d2f7b2273f807fd4d227c078c
watchoutfreedom <watchoutfreedom@gmail.com>
2023-05-26 13:13:11 +0200

    comments styles

--- web/src/components/feed/PostMessage/index.tsx
+++ web/src/components/feed/PostMessage/index.tsx
@@ -4,28 +4,27 @@ import { readableTimeLeftToDate } from "shared/date.utils";
 export default function PostMessage({ post }) {
   return (
     <>
-      <div className="card-notification__paragraph">
-        {post.message}
-      </div>
-      <div className="card-notification__header">
-        <h2 className="card-notification__title"></h2>
-      </div>
-      <div className="card-notification__date card-notification__date--nflex">
-        <span>{readableTimeLeftToDate(post.created_at)}</span>
-      </div>
-      <div className="card-notification__avatar">
-        <div className="avatar-small">
-          <ImageWrapper
-            imageType={ImageType.avatar}
-            src={post.author.avatar}
-            alt="Avatar"
-          />
-        </div>{' '}
-        {post.author.name}{' '}
-        <span style={{ color: 'gray' }}>
-          @{post.author.username}
-        </span>
-      </div>
+          <div className="card-notification__avatar"> 
+            <div className="avatar-small">
+              <ImageWrapper
+                imageType={ImageType.avatar}
+                src={post.author.avatar}
+                alt="Avatar"
+              />
+            </div>
+          </div>
+          <div className="card-notification__text">   
+            <div className="card-notification__header"> 
+              <div class="card-notification__info"> <div class="card-notification__name">{post.author.name}</div>{' '}@{post.author.username}</div>
+              <div className="card-notification__date card-notification__date--nflex">
+                <span>{readableTimeLeftToDate(post.created_at)}</span>
+              </div>  
+            </div>
+            <h2 className="card-notification__title"></h2>
+            <div className="card-notification__paragraph">
+              {post.message}
+            </div>    
+          </div>        
     </>
   );
 }
--- web/src/elements/Form/FormSubmit/index.tsx
+++ web/src/elements/Form/FormSubmit/index.tsx
@@ -1,7 +1,9 @@
+import Btn from "elements/Btn";
+
 ///default form element that can be customized with label, input, checkbox, description, note, etc. 
 export default function FormSubmit({isSubmitting, title, classNameExtra}
     ) {
-        const resultedClass = classNameExtra || "popup__options-btn btn-menu-white"
+        const resultedClass = classNameExtra || "popup__options-btn"
         return (
             <button type="submit" disabled={isSubmitting} className={resultedClass}>
                 {isSubmitting && <span className=""></span>}
