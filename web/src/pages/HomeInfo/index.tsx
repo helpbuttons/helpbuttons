e6b77dde62424c442698fe0e9d701f4134f6c4b7
Albatroz Jeremias <ajeremias@disroot.org>
2023-07-13 19:42:40 +0100

    feat: make types clickable

--- web/src/pages/HomeInfo/index.tsx
+++ web/src/pages/HomeInfo/index.tsx
@@ -25,7 +25,7 @@ import {  useState } from 'react';
 import { buttonColorStyle, buttonTypes } from 'shared/buttonTypes';
 import AdvancedFilters from 'components/search/AdvancedFilters';
 import { useToggle } from 'shared/custom.hooks';
-import { UpdateFiltersToFilterTag } from 'state/Explore';
+import { UpdateFiltersToFilterButtonType, UpdateFiltersToFilterTag } from 'state/Explore';
 import Alert from 'components/overlay/Alert';
 
 
@@ -41,6 +41,11 @@ export default function HomeInfo({
     router.push('/Explore')
   };
 
+  const filterButtonType = (buttonType) => {
+    store.emit(new UpdateFiltersToFilterButtonType(buttonType));
+    router.push('/Explore')
+  };
+  
   const currentUser = useStore(
     store,
     (state: GlobalState) => state.loggedInUser,
@@ -148,6 +153,7 @@ export default function HomeInfo({
                               btnType={BtnType.filter}
                               iconLeft={IconType.green}
                               caption={buttoTypeCountText}
+                              onClick={() => filterButtonType(buttonType.name)}
                             />
                           </div>
                         );
