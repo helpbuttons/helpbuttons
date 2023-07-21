0545e784a383e8d5c5cc7756a30a127616fe8321
watchoutfreedom <watchoutfreedom@gmail.com>
2023-07-21 17:34:22 +0200

    form fix

--- web/src/components/feed/CardNotification/index.tsx
+++ web/src/components/feed/CardNotification/index.tsx
@@ -42,7 +42,7 @@ export default function CardNotification({ activity = {} }) {
       const post = JSON.parse(activity.data);
       return (
         <NotificationCard
-          title={'New post'}
+          title={'New user update'}
           image={post.button.image}
           date={post.created_at}
           id={post.button.id}
