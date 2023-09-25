c5eeac89a6aedf828ccf5c19c956c32a918b0bbc
Albatroz Jeremias <ajeremias@disroot.org>
2023-09-25 18:00:44 +0100

    fix: merge migrations

--- api/src/data/migrations/1695230220733-add-price.ts
+++ api/src/data/migrations/1695230220733-add-price.ts
@@ -5,10 +5,12 @@ export class AddPrice1695230220733 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "button" ADD "price" double precision NOT NULL DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "network" ADD "currency" character varying NOT NULL DEFAULT 'EUR'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "button" DROP COLUMN "price"`);
+        await queryRunner.query(`ALTER TABLE "network" DROP COLUMN "currency"`);
     }
 
 }
