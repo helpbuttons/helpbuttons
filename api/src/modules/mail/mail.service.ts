4d6650c9ed34b53ddac285b5e953e88ff3f88a84
Albatroz Jeremias <ajeremias@disroot.org>
2023-11-08 13:29:38 +0000

    feat: translate all mails

--- api/src/modules/mail/mail.service.ts
+++ api/src/modules/mail/mail.service.ts
@@ -5,6 +5,8 @@ import { NetworkService } from '../network/network.service';
 import { configFileName } from '@src/shared/helpers/config-name.const';
 import { Queue } from 'bull';
 import { InjectQueue } from '@nestjs/bull';
+import translate from '@src/shared/helpers/i18n.helper';
+import { getUrl } from '@src/shared/helpers/mail.helper';
 const config = require(`../../..${configFileName}`);
 
 @Injectable()
@@ -17,43 +19,61 @@ export class MailService {
 
   sendActivationEmail({
     to,
-    cc,
-    bcc,
+    locale,
     activationUrl,
   }: {
     to: string;
-    cc?: string;
-    bcc?: string;
+    locale: string;
     activationUrl: string;
   }) {
-    return this.sendMail({
-      to,
-      cc,
-      bcc,
-      subject: 'Please verify your account',
-      template: 'activation-account',
-      context: { activationUrl },
+    return this.networkService.findDefaultNetwork().then((network) => {
+      return this.sendWithLink({
+        to,
+        content: translate(
+          locale,
+          'email.activateContent',
+          [network.name]
+        ),
+        subject: translate(
+          locale,
+          'email.activateSubject'
+        ),
+        link: getUrl(locale, activationUrl),
+        linkCaption: translate(
+          locale,
+          'email.activateLinkCaption'
+        ),
+      });
     });
   }
 
   sendLoginTokenEmail({
     to,
-    cc,
-    bcc,
     activationUrl,
+    locale,
   }: {
     to: string;
-    cc?: string;
-    bcc?: string;
     activationUrl: string;
+    locale: string
   }) {
-    return this.sendMail({
-      to,
-      cc,
-      bcc,
-      subject: 'You requested a one-click login',
-      template: 'login-token',
-      context: { activationUrl },
+    return this.networkService.findDefaultNetwork().then((network) => {
+      return this.sendWithLink({
+        to,
+        content: translate(
+          locale,
+          'email.loginTokenContent',
+          [network.name]
+        ),
+        subject: translate(
+          locale,
+          'email.loginTokenSubject'
+        ),
+        link: getUrl(locale, activationUrl),
+        linkCaption: translate(
+          locale,
+          'email.loginTokenLinkCaption'
+        ),
+      });
     });
   }
   
