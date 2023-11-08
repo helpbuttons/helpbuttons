7cdb6defb8bbb206da779d450c900775ffe405b6
Albatroz Jeremias <ajeremias@disroot.org>
2023-11-08 17:11:51 +0000

    feat: option to send mail directly if redis is not running

--- api/src/modules/mail/mail.processor.ts
+++ api/src/modules/mail/mail.processor.ts
@@ -4,6 +4,7 @@ import { NetworkService } from '../network/network.service';
 import { MailerService } from '@nestjs-modules/mailer';
 import { configFileName } from '@src/shared/helpers/config-name.const';
 import { GlobalVarHelper } from '@src/shared/helpers/global-var.helper';
+import { MailService } from './mail.service';
 const config = require(`../../..${configFileName}`);
 export interface MailJob {
     to: string;
@@ -17,50 +18,12 @@ export interface MailJob {
 export class MailConsumer {
   constructor(
     private readonly networkService: NetworkService,
-    private readonly mailerService: MailerService,
+    private readonly mailService: MailService,
   ) {}
 
   @Process()
   async sendMail(job: Job<MailJob>) {
-    if (!GlobalVarHelper.smtpAvailable) {
-      console.log(
-        'Error when smtp not working. mail could not be sent',
-      );
-      return Promise.reject('Error when smtp not working. mail could not be sent');
-    }
-    const {to, cc, bcc, subject, template, context} = job.data;
-    return this.networkService
-      .findDefaultNetwork()
-      .then((network) => {
-        let from  = config.from
-        try {
-          from = network.name + config.from.slice(config.from.indexOf('<'))
-        }catch(err)
-        {
-          console.log('could not add network name to from')
-        }
-
-        return this.mailerService
-          .sendMail({
-            to,
-            cc,
-            bcc,
-            from: from,
-            subject: subject,
-            template,
-            context: {...context, hostName: config.hostName},
-          })
-          .then((mail) => {
-            console.log(
-              `>> mail sent to ${to} with template '${template}'`,
-            );
-          })
-          .catch((error) => {
-            console.log(error);
-            console.trace();
-          });
-      })
-      .catch((error) => console.log('getting network error?'));
+    return this.mailService.sendMailDirectly(job.data)
   }
 
   @OnQueueFailed()
