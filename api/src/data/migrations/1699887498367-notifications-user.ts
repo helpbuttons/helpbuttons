436b54a77520a7bb1d0928d8d2892c246e69c73c
Albatroz Jeremias <ajeremias@disroot.org>
2023-11-14 12:18:22 +0000

    feat: merge migrations

--- api/src/data/migrations/1699887498367-notifications-user.ts
+++ api/src/data/migrations/1699887498367-notifications-user.ts
@@ -8,6 +8,7 @@ export class NotificationsUser1699887498367 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "user" ADD "address" character varying NOT NULL default ''`);
         await queryRunner.query(`ALTER TABLE "user" ADD "center" geography`);
         await queryRunner.query(`ALTER TABLE "user" ADD "radius" integer NOT NULL DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "activity" ADD "outbox" boolean NOT NULL DEFAULT false`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -15,6 +16,7 @@ export class NotificationsUser1699887498367 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "center"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "address"`);
         await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "tags"`);
+        await queryRunner.query(`ALTER TABLE "activity" DROP COLUMN "outbox"`);
     }
 
 }
