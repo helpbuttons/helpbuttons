b3cb5e9e33def0c8623e347a7fba0563fae5c7e2
watchoutfreedom <watchoutfreedom@gmail.com>
2024-03-02 22:44:51 +0100

    Error page

--- web/src/pages/Error/index.tsx
+++ web/src/pages/Error/index.tsx
@@ -1,6 +1,9 @@
 //ERROR PAGE
 import NavBottom from 'components/nav/NavBottom';
 import ErrorMessage from '../../components/overlay/ErrorMessage'
+import Popup from 'components/popup/Popup';
+import router from 'next/router';
+import t from 'i18n';
 
 
 
@@ -8,7 +11,16 @@ export default function Error() {
 
   return (
     <>
-      This page could not be found.
+      <Popup
+        title={t('common.error')}
+        linkBack={() => router.back()}    
+        >        
+        <div className='error__message'>
+
+          {t('error.notFoundMessage')}
+
+        </div>
+      </Popup>
       <NavBottom loggedInUser={null}/>
     </>
   );
