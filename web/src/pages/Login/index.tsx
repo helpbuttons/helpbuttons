37d9974b89900dd6af50de11b50aff3174a7f51c
Albatroz Jeremias <ajeremias@disroot.org>
2024-03-15 18:38:47 +0000

    fix: on first login redirect to homeinfo. on logout change locale to network locale

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -8,9 +8,22 @@ import PopupSection from 'components/popup/PopupSection';
 import LoginForm from 'components/user/LoginForm';
 import t from 'i18n';
 import router from 'next/router';
+import { GlobalState, store } from 'pages';
+import { useEffect } from 'react';
+import { useStore } from 'store/Store';
 export default Login;
 
 function Login() {
+    const loggedInUser = useStore(
+      store,
+      (state: GlobalState) => state.loggedInUser,
+    );
+    useEffect(() => {
+        if(loggedInUser)
+        {
+            router.push({pathname: '/HomeInfo'}, null, { locale: loggedInUser.locale })
+        }
+    }, [loggedInUser])
     return (
         <Popup title={t("user.login")} linkBack={() => router.back()}>
            <LoginForm/>
