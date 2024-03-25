4a7735d8731eb277c57d12c74c0f830ec3b51c2f
Albatroz Jeremias <ajeremias@disroot.org>
2024-03-25 17:40:20 +0000

    fix: button file api call

--- web/src/pages/ButtonFile/[id].tsx
+++ web/src/pages/ButtonFile/[id].tsx
@@ -57,7 +57,7 @@ export const getServerSideProps = async (ctx: NextPageContext) => {
     'New Button',
     ctx,
   );
-  const buttonUrl = `${process.env.API_URL}/buttons/findById/${ctx.params.id}`;
+  const buttonUrl = `${process.env.API_URL}buttons/findById/${ctx.params.id}`;
   const currentButtonFetch = await fetch(buttonUrl, {
     next: { revalidate: 10 },
   });
