87ad00e0f323343cbf5fdbec1d0d6587826cf224
Albatroz Jeremias <ajeremias@disroot.org>
2024-04-10 23:05:33 +0100

    fix: set deleted btns as expired btns

--- api/src/data/migrations/1712157121597-btn-expired.ts
+++ api/src/data/migrations/1712157121597-btn-expired.ts
@@ -5,6 +5,7 @@ export class BtnExpired1712157121597 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "button" ADD "expired" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`UPDATE "button" SET expired = true, deleted = false where deleted = true`);
         await queryRunner.query(`drop view network_button_types; 
         create view network_button_types AS select type,count(id) from button WHERE deleted = false AND expired = false group by type`);
     }
