e41c6ddb864febec410adbc68010aac91840dcd4
Albatroz Jeremias <ajeremias@disroot.org>
2024-04-15 23:40:55 +0100

    fix: compilation error because of depecrated map markers

--- web/src/components/map/CardButtonMap/index.tsx
+++ web/src/components/map/CardButtonMap/index.tsx
@@ -1,5 +1,5 @@
 //The smallest of buttons'cards. It ddisplays over the map when a marker is clicked / touched. Itt was very reduce info, like image, tags, title or other fieldds conidered by the buttonTemplate field.
-import { CardButtonHeadSmall } from "components/button/CardButton";
+// import { CardButtonHeadSmall } from "components/button/CardButton";
 import ImageWrapper, { ImageType } from "elements/ImageWrapper";
 import { buttonColorStyle } from "shared/buttonTypes";
 import { IoChevronForwardOutline } from "react-icons/io5";
@@ -10,7 +10,8 @@ export default function CardButtonMap({ button }) {
   return (
     <div style={buttonColorStyle(cssColor)}>
       <div className="card-button-map">
-        <CardButtonHeadSmall button={button}/>
+        missing card button head small
+        {/* <CardButtonHeadSmall button={button}/> */}
         <div className="card-button-map__picture-container">
           <div className="card-button-map__nav">
             <div className="arrow btn-circle__icon">
