679db20ab6711db1072b2bde0c8e71a6c83a4b76
watchoutfreedom <watchoutfreedom@gmail.com>
2024-06-15 17:40:29 +0200

    css list items

--- web/src/components/list/List/index.tsx
+++ web/src/components/list/List/index.tsx
@@ -147,7 +147,7 @@ function List({
           {!showAdvancedFilters && (
             <DraggableList
               className={'list__container '}
-              onScroll={handleScrollHeight}
+              // onScroll={handleScrollHeight}
               initialPos={{
                 x: 0,
                 y: window.innerHeight - 110,
@@ -203,7 +203,7 @@ function List({
               </div>
               <div
                 className={
-                  'list__content list__content--full-screen '
+                  'list__content list__content--full-screen' 
                 }
                 onScroll={handleScrollWidth}
               >
@@ -225,7 +225,7 @@ function List({
               'list__container ' +
               (showMap ? '' : ' list__container--full-screen')
             }
-            onScroll={handleScrollHeight}
+            // onScroll={handleScrollHeight}
           >
             <div
               className={
