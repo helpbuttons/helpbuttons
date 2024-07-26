eb3957c4120aa0e4452de316a478665005c1cf1f
Albatroz Jeremias <ajeremias@disroot.org>
2024-07-26 13:45:40 +0100

    feat: remove uppercase for all, refator buttontypes navbar

--- web/src/components/nav/ButtonTypes/index.tsx
+++ web/src/components/nav/ButtonTypes/index.tsx
@@ -1,53 +1,78 @@
-import Btn, { BtnCaption } from "elements/Btn";
-import router from "next/router";
-import { GlobalState, store } from "pages";
-import { useButtonTypes } from "shared/buttonTypes";
-import { ToggleAdvancedFilters, UpdateFiltersToFilterButtonType, updateCurrentButton } from "state/Explore";
-import { useStore } from "store/Store";
+import { BtnCaption } from 'elements/Btn';
+import router from 'next/router';
+import { GlobalState, store } from 'pages';
+import { useEffect, useState } from 'react';
+import {
+  ToggleAdvancedFilters,
+  UpdateFiltersToFilterButtonType,
+  updateCurrentButton,
+} from 'state/Explore';
+import { useStore } from 'store/Store';
 
 export function ListButtonTypes({ selectedNetwork, pageName }) {
+  const showAdvancedFilters = useStore(
+    store,
+    (state: GlobalState) => state.explore.map.showAdvancedFilters,
+    false,
+  );
 
-    const showAdvancedFilters = useStore(
-      store,
-      (state: GlobalState) => state.explore.map.showAdvancedFilters,
-      false
-    );
+  const [buttonTypes, setButtonTypes] = useState([]);
+  useEffect(() => {
+    if (buttonTypes) {
+      if (selectedNetwork) {
+        setButtonTypes(() => {
+          return selectedNetwork.buttonTemplates.map((buttonType) => {
+            const typeCount = selectedNetwork.buttonTypesCount.find(
+              (buttonTypeCount) =>
+                buttonTypeCount.type == buttonType.name,
+            );
+            if (typeCount) {
+              return { ...buttonType, count: typeCount.count };
+            }
+            return buttonType;
+          });
+        });
+      } else {
+        setButtonTypes(() => {
+          return selectedNetwork.buttonTemplates;
+        });
+      }
+    }
+  }, [selectedNetwork]);
 
-    const filterButtonType = (buttonType) => {
-      store.emit(new ToggleAdvancedFilters(false))
-      store.emit(new UpdateFiltersToFilterButtonType(buttonType));
-      store.emit(new updateCurrentButton(null))
-      if(pageName != 'Explore')
-      {
-        router.push('/Explore');
-      } 
-      
-    };
-    const buttonTypes = useButtonTypes();
-  
-    return (
-      <>
-        {buttonTypes && buttonTypes.map((buttonType, idx) => {
-          const buttonTypeFound = selectedNetwork.buttonTypesCount.find(
-            (buttonTypeCount) =>
-              buttonTypeCount.type == buttonType.name,
-          );
+  return (
+    <ButtonTypesNav buttonTypes={buttonTypes} pageName={pageName} />
+  );
+}
+
+export function ButtonTypesNav({ buttonTypes, pageName = '' }) {
+  const filterButtonType = (buttonType) => {
+    store.emit(new ToggleAdvancedFilters(false));
+    store.emit(new UpdateFiltersToFilterButtonType(buttonType));
+    store.emit(new updateCurrentButton(null));
+    if (pageName != 'Explore') {
+      router.push('/Explore');
+    }
+  };
+  return (
+    <>
+      {buttonTypes &&
+        buttonTypes.map((buttonType, idx) => {
           const buttoTypeCountText =
-            (buttonTypeFound?.count
-              ? buttonTypeFound?.count
-              : 0
-            ).toString() +
+            (buttonType?.count ? buttonType?.count : 0).toString() +
             ' ' +
             buttonType.caption;
           return (
-            <div
-              className="hashtags__list-item"
-              key={idx}
-            >
-              <BtnCaption caption={buttoTypeCountText} icon={buttonType?.icon} color={buttonType.cssColor} onClick={() => filterButtonType(buttonType.name)}/>
+            <div className="hashtags__list-item" key={idx}>
+              <BtnCaption
+                caption={buttoTypeCountText}
+                icon={buttonType?.icon}
+                color={buttonType.cssColor}
+                onClick={() => filterButtonType(buttonType.name)}
+              />
             </div>
           );
         })}
-      </>
-    );
-  }
+    </>
+  );
+}
