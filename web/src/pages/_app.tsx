c54234b5a2a14c78c73d2c5bdfd172f3daf6fd02
Albatroz Jeremias <ajeremias@disroot.org>
2024-10-01 23:20:04 +0100

    fix: hide notifications request button

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -10,7 +10,7 @@ import { GlobalState, store } from 'pages';
 import { useConfig, useSelectedNetwork } from 'state/Networks';
 import { FetchUserData, LoginToken } from 'state/Users';
 
-import { useGlobalStore, useStore } from 'store/Store';
+import { useStore } from 'store/Store';
 import { alertService } from 'services/Alert';
 import { SetupSteps } from '../shared/setupSteps';
 
@@ -27,18 +27,8 @@ import NavHeader from 'components/nav/NavHeader';
 import { ShowDesktopOnly, ShowMobileOnly } from 'elements/SizeOnly';
 import SEO from 'components/seo';
 import { LoadabledComponent } from 'components/loading';
-import {
-  ActivityMarkAllAsRead,
-  PermissionGranted,
-  PermissionRevoke,
-} from 'state/Activity';
-import { IoNotificationsOutline } from 'react-icons/io5';
-import Btn, {
-  BtnType,
-  ContentAlignment,
-  IconType,
-} from 'elements/Btn';
 import MainPopup from 'components/popup/Main/';
+import { DesktopNotifications } from 'components/notifications';
 
 export default appWithTranslation(MyApp);
 
@@ -324,83 +314,3 @@ export const ClienteSideRendering = ({ children }) => {
   return <>{isClient && children}</>;
 };
 
-
-export function DesktopNotificationsButton() {
-  const hasNotificationPermissions = useGlobalStore(
-    (state: GlobalState) =>
-      state.activitesState.notificationsPermissionGranted,
-  );
-
-  const requestPermission = () => {
-    const isSupported = () =>
-      'Notification' in window &&
-      'serviceWorker' in navigator &&
-      'PushManager' in window;
-
-    if (isSupported()) {
-      Notification.requestPermission().then(function (getperm) {
-        if (getperm == 'granted') {
-          console.log(getperm);
-          store.emit(new PermissionGranted());
-        } else {
-          store.emit(new PermissionRevoke());
-        }
-      });
-    }
-  };
-  return (
-    <>
-      {!hasNotificationPermissions && (
-        <Btn
-          btnType={BtnType.filterCorp}
-          iconLink={<IoNotificationsOutline />}
-          caption={t('homeinfo.notificationsPermission')}
-          iconLeft={IconType.circle}
-          contentAlignment={ContentAlignment.center}
-          onClick={requestPermission}
-        />
-      )}
-    </>
-  );
-}
-
-function DesktopNotifications() {
-  const hasNotificationPermissions = useGlobalStore(
-    (state: GlobalState) =>
-      state.activitesState.notificationsPermissionGranted,
-  );
-
-  const activities = useStore(
-    store,
-    (state: GlobalState) => state.activitesState.activities,
-  );
-
-  const notificationsShown = useRef(false);
-  useEffect(() => {
-    if (
-      activities &&
-      activities.length > 0 &&
-      !notificationsShown.current
-    ) {
-      notificationsShown.current = true;
-
-      activities
-        .filter((activity) => !activity.read)
-        .slice(0, 5)
-        .map((activity: ActivityDtoOut) => {
-          alertService.info(activity.title);
-        });
-      // mark as read, if permissions to send notifications are given, dunno if best behavior.
-      if (hasNotificationPermissions) {
-        if (
-          activities.filter((activity) => !activity.read).length > 0
-        ) {
-          alertService.info(t('activities.markedAllAsRead'));
-          store.emit(new ActivityMarkAllAsRead());
-        }
-      }
-    }
-  }, [activities]);
-
-  return <></>;
-}
