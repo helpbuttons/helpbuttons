ffdceb8b407815ee67459c3d084df5c7fc46be7a
watchoutfreedom <watchoutfreedom@gmail.com>
2024-12-18 17:29:39 +0200

    attributions and css fix

--- web/src/components/brand/powered.tsx
+++ web/src/components/brand/powered.tsx
@@ -6,7 +6,7 @@ export function PoweredBy() {
   return (
     <>
       {t('homeinfo.powered')}{' '}
-      <a href="https://helpbuttons.org">helpbuttons</a>
+      <a href="https://helpbuttons.org">Helpbuttons</a>
     </>
   );
 }
@@ -18,11 +18,10 @@ export function PoweredExtra() {
   return (
     <>
       <PoweredBy/>
-      <a href={`https://github.com/helpbuttons/helpbuttons/releases/tag/${metadata.version}`}>{metadata.version}</a> - 
-      <a href="/Faqs">{t('menu.faqs')}</a> -
+      <a href={`https://github.com/helpbuttons/helpbuttons/releases/tag/${metadata.version}`}> {metadata.version}</a> - 
       {/* <a href={`/Contact`}>Contact</a> - 
       <a href="https://helpbuttons.org/help">Help</a> - */}
-      <a href={`/api`}>API</a> - 
+      <a href={`/api`}> API </a> 
       <License/>
     </>
   );
@@ -31,7 +30,15 @@ export function PoweredExtra() {
 function License() {
   return (
     <a href="https://github.com/helpbuttons/helpbuttons/blob/main/LICENSE">
-      GNU Affero General Public License v3.0
+      GNU Affero license v3.0
+    </a>
+  );
+}
+
+export function LegalRepresentation() {
+  return (
+    <a href="https://wofreedom.org">
+    {' '}{t('common.by')} Wof! - 2024
     </a>
   );
 }
