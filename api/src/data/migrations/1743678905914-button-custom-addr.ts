80454a9cd189f586e6b32bfbdd9913c0e41ee91a
Albatroz Jeremias <ajeremias@disroot.org>
2025-04-03 15:21:56 +0100

    fix: migrations failures order on custom address

--- api/src/data/migrations/1743678905914-button-custom-addr.ts
+++ api/src/data/migrations/1743678905914-button-custom-addr.ts
@@ -5,10 +5,14 @@ export class ButtonCustomAddr1743678905914 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "button" ADD "isCustomAddress" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`drop view custom_places`);
+        await queryRunner.query(`create view custom_places as select latitude,longitude,address,location,hexagon from button where "isCustomAddress" = true;`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "button" DROP COLUMN "isCustomAddress"`);
+        await queryRunner.query(`drop view custom_places`);
+        await queryRunner.query(`create view custom_places as select latitude,longitude,address,location,hexagon from button`);
     }
 
 }
