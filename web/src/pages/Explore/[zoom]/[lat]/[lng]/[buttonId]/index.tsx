8524eb8db5aa81b782f9a363ae265430512c3d65
Albatroz Jeremias <ajeremias@disroot.org>
2025-09-17 11:23:15 +0100

    fix: center map to button center

--- web/src/pages/Explore/[zoom]/[lat]/[lng]/[buttonId]/index.tsx
+++ web/src/pages/Explore/[zoom]/[lat]/[lng]/[buttonId]/index.tsx
@@ -3,7 +3,7 @@ import { setMetadata } from 'services/ServerProps';
 import t from 'i18n';
 import { useGlobalStore } from 'state';
 import { GlobalState, store } from 'state';
-import { ExploreSettings, FindButton, updateCurrentButton, UpdateExploreSettings } from 'state/Explore';
+import { ExploreSettings, FindButton, updateCurrentButton, UpdateExploreSettings, UpdateHexagonClicked } from 'state/Explore';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { markerFocusZoom } from 'components/map/Map/Map.consts';
@@ -11,6 +11,7 @@ import { ExplorePage } from 'pages/Explore';
 import { ErrorName } from 'shared/types/error.list';
 import { alertService } from 'services/Alert';
 import { useSelectedNetwork } from 'state/Networks';
+import { cellToZoom } from 'shared/honeycomb.utils';
 
 export default function Explore({
     metadata
@@ -18,12 +19,21 @@ export default function Explore({
     const router = useRouter();
     const selectedNetwork = useSelectedNetwork()
 
+    const centerMapToButton = (button) => {
+      const _updateSettings: Partial<ExploreSettings> = {
+        center: [button.latitude, button.longitude],
+        zoom: markerFocusZoom,
+      }
+      store.emit(new UpdateExploreSettings(_updateSettings));
+    }
+
     const { buttonId, zoom, lat, lng } = router.query;
     const currentButton = useGlobalStore((state: GlobalState) => state.explore.currentButton)
     useEffect(() => {
         if(buttonId && (!currentButton || currentButton?.id != buttonId))
         {
           store.emit(new FindButton(String(buttonId), (button) => {
+            centerMapToButton(button)
             store.emit(new updateCurrentButton(button))
           }, (error) => {
             if(error.errorName == ErrorName.ButtonNotFound)
@@ -32,6 +42,8 @@ export default function Explore({
               router.push(`/Explore/${selectedNetwork.exploreSettings.zoom}/${selectedNetwork.exploreSettings?.center[0]}/${selectedNetwork.exploreSettings?.center[1]}`, undefined, { shallow: true });
             }
           }))
+        }else if(currentButton){
+          centerMapToButton(currentButton)
         }
       }, [buttonId])
     return <ExplorePage/>
