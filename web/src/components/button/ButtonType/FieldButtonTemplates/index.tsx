608c12d9ba7e64692291696f0e425c1dc45df0f6
watchoutfreedom <watchoutfreedom@gmail.com>
2025-10-13 16:24:30 +0200

    fix css types

--- web/src/components/button/ButtonType/FieldButtonTemplates/index.tsx
+++ web/src/components/button/ButtonType/FieldButtonTemplates/index.tsx
@@ -253,7 +253,7 @@ function ButtonTemplateForm({ label, explain, append }) {
   const openPopup = () => setShowPopup(() => true);
   const customFields = watch('customFields')
   return (
-      <PickerField btnType={BtnType.corporative} iconLeft={<IoAdd/>} label={''} explain={''} btnLabel={label} showPopup={showPopup} openPopup={openPopup} closePopup={closePopup}>
+      <PickerField btnType={BtnType.corporative}  iconLeft={IconType.svg} iconLink={<IoAdd/>} label={''} explain={''} btnLabel={label} showPopup={showPopup} openPopup={openPopup} closePopup={closePopup}>
         {/* headerText={t('configuration.setType')} */}
         <>
         {/* <div className="form__input--button-type-field"></div> */}
@@ -297,6 +297,9 @@ function ButtonTemplateForm({ label, explain, append }) {
         }
         <Btn
           caption={t('configuration.addType')}
+          iconLeft={IconType.svg}
+          iconLink={<IoAdd/>}
+          contentAlignment={ContentAlignment.center}
           onClick={() => onAddNewButtonTemplate(getValues())}
           btnType={BtnType.submit}
         />
