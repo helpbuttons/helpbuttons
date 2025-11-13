18f17aeb9d671bdeef64f2dfaa172fe7706d2860
watchoutfreedom <watchoutfreedom@gmail.com>
2025-11-13 13:26:54 +0100

    css list

--- web/src/elements/Dropdown/Dropdown/index.tsx
+++ web/src/elements/Dropdown/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-///dropddown selector component
+///dropdown selector component
 import React, { useState } from 'react';
 import { IoList } from 'react-icons/io5';
 export function DropdownField({
@@ -18,6 +18,7 @@ export function DropdownField({
     </div>
   );
 }
+
 export function Dropdown({
   options,
   onChange = (value) => {},
@@ -35,24 +36,22 @@ export function Dropdown({
   };
 
   let selectAttrs = {
-    className: className,
+    className: `${className} dropdown-select-with-icon`,
     onChange: handleChange,
     defaultValue: defaultSelected,
   }
   if(!defaultSelected)
   {
     selectAttrs = {
-      className: className,
+      className: `${className} dropdown-select-with-icon`,
       onChange: handleChange,
       defaultValue: false,
     }
   }
   return (
-    <>
-      <select
-        {...selectAttrs}
-      > 
-
+    <div className="dropdown-select__container">
+      <IoList className="dropdown-select__icon" />
+      <select {...selectAttrs}>
         {options.map((option, index) => (
           <option
             className="dropdown-select__option"
@@ -63,6 +62,6 @@ export function Dropdown({
           </option>
         ))}
       </select>
-    </>
+    </div>
   );
 }
