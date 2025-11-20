7980615a1583e57df3cca07eda72309399bb814b
watchoutfreedom <watchoutfreedom@gmail.com>
2025-11-20 11:07:30 +0100

    color contrast

--- web/src/shared/helpers/color.helper.tsx
+++ web/src/shared/helpers/color.helper.tsx
@@ -1,6 +1,6 @@
 export function getReadableTextColor(hexBackgroundColor) {
   if (!hexBackgroundColor || hexBackgroundColor.length < 4) {
-    return '#000000';
+    return '#0E0E0E';
   }
 
   let hex = hexBackgroundColor.substring(1);
@@ -9,7 +9,7 @@ export function getReadableTextColor(hexBackgroundColor) {
   }
 
   if (hex.length !== 6) {
-    return '#000000';
+    return '#0E0E0E';
   }
 
   const r = parseInt(hex.substring(0, 2), 16);
@@ -18,5 +18,5 @@ export function getReadableTextColor(hexBackgroundColor) {
 
   const brightness = ((r * 299) + (g * 587) + (b * 114)) / 1000;
 
-  return brightness > 128 ? '#000000' : '#FFFFFF';
+  return brightness > 128 ? '#0E0E0E' : '#f0f0f0';
 }
