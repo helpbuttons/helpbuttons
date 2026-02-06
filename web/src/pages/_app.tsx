a85f09924b223c58598a55a1d87fb990116caf2f
watchoutfreedom <watchoutfreedom@gmail.com>
2026-02-06 11:29:44 +0100

    Revert "remove registered required alert"
    This reverts commit 579fad95bdde4f2071c37cdbf9abda95a33fb2b4.

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -194,6 +194,7 @@ function MyApp({ Component, pageProps }) {
 
     if (!isAllowed) {
       console.log('not allowd')
+      alertService.error(t('common.registeredRequired'));
       store.emit(new ResetFilters()) // TODO: bug when using router.back
       store.emit(new SetMainPopup(MainPopupPage.LOGIN))
       router.back()
