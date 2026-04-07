0ea779356a29e60e1976bd0d2a9b31e111c42523
Albatroz Jeremias <ajeremias@disroot.org>
2026-04-07 23:01:15 +0100

    fix: sending message to button 404

--- web/src/components/feed/Activities/Activity.tsx
+++ web/src/components/feed/Activities/Activity.tsx
@@ -159,7 +159,7 @@ export default function ActivitiesUser({ activityId =null, draft = false, select
         }
         {((selectedActivity) || draft) &&
           <div className="feed-section__center">
-            <ActivityButton setSelectedActivity={setSelectedActivity} closeConversation={closeConversation} selectedActivity={selectedActivity} isDrafting={draft} selectedButton={sideBarButton} />
+            <ActivityButton setSelectedActivity={setSelectedActivity} closeConversation={closeConversation} selectedActivity={selectedActivity} isDrafting={draft} selectedButton={sideBarButton ? currentButton : null} />
             <div className="feed-section__center__chat"></div>
           </div>
         }
