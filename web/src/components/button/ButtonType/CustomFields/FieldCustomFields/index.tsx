fcc3e9130b41252f41032911ec508010ce1f0917
watchoutfreedom <watchoutfreedom@gmail.com>
2026-04-18 19:27:37 +0200

    form last

--- web/src/components/button/ButtonType/CustomFields/FieldCustomFields/index.tsx
+++ web/src/components/button/ButtonType/CustomFields/FieldCustomFields/index.tsx
@@ -1,11 +1,18 @@
 import { findCustomTemplate } from 'components/templates';
+import { CustomFields } from 'shared/types/customFields.type';
 import React from 'react';
 
 export default function FieldCustomFields(
 props) {
 
   const renderFields = () => {
-    return props?.customFields.map((fieldProps, key) => {
+    //set sheduler as last field of the form
+    const sortedFields = [...(props?.customFields ?? [])].sort((a, b) => {
+      if (a.type === CustomFields.Scheduler) return 1;
+      if (b.type === CustomFields.Scheduler) return -1;
+      return 0;
+    });
+    return sortedFields.map((fieldProps, key) => {
       const type = fieldProps.type;
       const FieldComponent = findCustomTemplate(type)?.templateField;
       
