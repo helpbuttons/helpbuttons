3099cb058b6e41084f72abbfdbe514d485ea404a
Albatroz Jeremias <ajeremias@disroot.org>
2026-06-01 11:15:14 +0100

    feat: hide country is default

--- api/src/data/migrations/1779220638873-hidecountry.ts
+++ api/src/data/migrations/1779220638873-hidecountry.ts
@@ -4,7 +4,7 @@ export class Hidecountry1779220638873 implements MigrationInterface {
     name = 'Hidecountry1779220638873'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "network" ADD "hideCountryOnAddresses" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "network" ADD "hideCountryOnAddresses" boolean NOT NULL DEFAULT true`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
